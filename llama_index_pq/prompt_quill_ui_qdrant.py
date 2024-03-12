17df174a1b516b6eb2a58256f384a5fab13e99c6
osiwrx <osi1880vr@gmail.com>
2024-03-12 17:32:02 +0100

    fixed some url issues

--- llama_index_pq/prompt_quill_ui_qdrant.py
+++ llama_index_pq/prompt_quill_ui_qdrant.py
@@ -14,6 +14,7 @@
 
 import gradio as gr
 import llm_interface_qdrant
+import os
 
 interface = llm_interface_qdrant.LLM_INTERFACE()
 
@@ -42,7 +43,7 @@ with gr.Blocks(css=css) as pq_ui:
 
 		with gr.Row():
 			# Image element (adjust width as needed)
-			gr.Image("logo/pq_v_small.jpg",width="20vw",show_label=False,show_download_button=False,container=False, elem_classes="gr-image",)
+			gr.Image(os.path.join(os.getcwd(),"logo/pq_v_small.jpg"),width="20vw",show_label=False,show_download_button=False,container=False, elem_classes="gr-image",)
 
 			# Title element (adjust font size and styling with CSS if needed)
 			gr.Markdown("**Prompt Quill**", elem_classes="app-title")  # Add unique ID for potential CSS styling
