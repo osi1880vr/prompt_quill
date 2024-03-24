938d14e2577770d857e485ab766e136408f3ccc7
osiwrx <osi1880vr@gmail.com>
2024-03-24 18:32:36 +0100

    fixed a bug with exit at the wrong place

--- llama_index_pq/start_qdrant.bat
+++ llama_index_pq/start_qdrant.bat
@@ -3,16 +3,11 @@ set CONDA_ROOT_PREFIX=%cd%\installer_files\conda
 set INSTALL_ENV_DIR=%cd%\installer_files\env
 set BASE_DIR=%cd%
 
-
-
 if exist "%INSTALL_DIR%\bin" (
     ECHO Setting CUDA environment
     set CUDA_PATH=%INSTALL_ENV_DIR%
     set CUDA_HOME=%INSTALL_ENV_DIR%
 )
-
-exit
-
 
 ECHO Startup Qdrant to upload the data
 cd %INSTALL_DIR%/qdrant
