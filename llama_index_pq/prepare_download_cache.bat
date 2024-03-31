27d2555e9aeeff05b3d2b209842477f113cf2c60
Werner Oswald <osiworx@gmail.com>
2024-03-31 11:14:57 +0200

    prepare a download cache for multiple reinstalls, mainly good for testing and saving bandwith. the one click installer will check for cached downloads and use them. the normal usecase just one click install does not change. if no cache is present the download will be done as usual and no cache will be created

--- llama_index_pq/prepare_download_cache.bat
+++ llama_index_pq/prepare_download_cache.bat
@@ -27,17 +27,29 @@ if not exist "%CACHE_DIR%/qdrant-x86_64-pc-windows-msvc.zip" (
     ECHO Download Qdrant Portable Version
     curl -L https://github.com/qdrant/qdrant/releases/download/v1.8.1/qdrant-x86_64-pc-windows-msvc.zip --output %CACHE_DIR%/qdrant-x86_64-pc-windows-msvc.zip
 )
+else
+(
+    ECHO skipped Download Qdrant Portable Version
+)
+
 
 if not exist "%CACHE_DIR%/dist-qdrant.zip" (
     ECHO Download Qdrant Web UI
     curl -L https://github.com/qdrant/qdrant-web-ui/releases/download/v0.1.22/dist-qdrant.zip --output %CACHE_DIR%/dist-qdrant.zip
+)
+else
+(
+    ECHO skipped Download Qdrant Web UI
 )
 
 if not exist "%CACHE_DIR%/data.zip" (
     ECHO Download LLama-index QDrant data
     curl -L https://civitai.com/api/download/models/407093 --output %CACHE_DIR%/data.zip
 )
-
+else
+(
+    ECHO skipped Download LLama-index QDrant data
+)
 
 
 :end
