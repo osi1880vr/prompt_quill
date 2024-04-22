6072b0e9696b9d97c80d1857b682a844795abf74
osiwrx <osi1880vr@gmail.com>
2024-04-22 16:27:34 +0200

    bug fix for refactoring imports

---
+++ llama_index_pq/start_prompt_quil_offline.bat
@@ -0,0 +1,38 @@
+cd /D "%~dp0"
+
+set HF_DATASETS_OFFLINE=1
+set TRANSFORMERS_OFFLINE=1
+
+set INSTALL_DIR=%cd%\installer_files
+set CONDA_ROOT_PREFIX=%cd%\installer_files\conda
+set INSTALL_ENV_DIR=%cd%\installer_files\env
+set BASE_DIR=%cd%
+
+
+if exist "%INSTALL_DIR%\bin" (
+    ECHO Setting CUDA environment
+    set CUDA_PATH=%INSTALL_ENV_DIR%
+    set CUDA_HOME=%INSTALL_ENV_DIR%
+)
+
+set "CUDA_PATH=%INSTALL_ENV_DIR%"
+set "CUDA_HOME=%CUDA_PATH%"
+
+
+ECHO Startup Qdrant to upload the data
+cd %INSTALL_DIR%/qdrant
+start "" "%INSTALL_DIR%/qdrant/qdrant.exe"  --disable-telemetry
+
+cd %BASE_DIR%
+REM we do this to give Qdrant some time to fire up
+
+@rem activate installer env
+call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" activate "%INSTALL_ENV_DIR%" || ( echo. && echo Miniconda hook not found. && goto end )
+
+start /W "" python pq/check_qdrant_up.py
+
+call python pq\prompt_quill_ui_qdrant.py
+
+
+pause
+exit
