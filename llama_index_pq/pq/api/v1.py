a80aac819fa02c18d864bd010327b910204cae84
Werner Oswald <osiworx@gmail.com>
2024-05-05 09:32:32 +0200

    API enabled

--- llama_index_pq/pq/api/v1.py
+++ llama_index_pq/pq/api/v1.py
@@ -1,19 +1,30 @@
 import time
 import threading
 import json
-from flask import Flask
+from flask import Flask, request, jsonify
+
 
 from llm_fw import llm_interface_qdrant
 interface = llm_interface_qdrant.get_interface()
 app = Flask(__name__)
 
-@app.route('/get_prompt')
+
+
+
+
+
+@app.route('/get_prompt', methods=['POST'])
 def hello_world():
-    return json.dumps(interface.run_api_llm_response('a nice cat'))
+    try:
+        data = request.json
+        return json.dumps(interface.run_api_llm_response(data['query']))
+    except:
+        return jsonify({'error': 'Invalid JSON format'}), 400
+
 
 
 def web():
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=64738)
 
 
 def run_api():
