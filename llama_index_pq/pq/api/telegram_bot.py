75cc19cc873fa48c8ae83e6669a2fcba99fd86db
Werner Oswald <osiworx@gmail.com>
2024-06-22 21:03:11 +0200

    added API for telegram bot

--- llama_index_pq/pq/api/telegram_bot.py
+++ llama_index_pq/pq/api/telegram_bot.py
@@ -24,16 +24,16 @@ class Telegram:
 				self.g.settings_data['automa_layerdiffuse_enable'] = True
 				self.g.settings_data["automa_width"] = 1024
 				self.g.settings_data["automa_height"] = 1024
+		else:
+			self.g.settings_data['automa_layerdiffuse_enable'] = False
+			self.g.settings_data["automa_width"] = data['w']
+			self.g.settings_data["automa_height"] = data['h']
 
 		image = self.automa_gen(prompt['prompt'])
-		if "layerdiffuse" in data:
-			if data['layerdiffuse']:
-				self.g.settings_data['automa_layerdiffuse_enable'] = False
 
 		return {"prompt": prompt['prompt'],
 				"negative_prompt": self.g.settings_data['negative_prompt'],
 				"image": image}
-
 
 
 	def get_prompt(self, data):
